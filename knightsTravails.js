6d333ca54488b17f49ec98a7dc4714be51878e97
arashad202 <ahm.rashad.202@gmail.com>
2025-07-09 05:43:29 +0300

    check every move from current and check if it reaches the end or dig to another level

--- knightsTravails.js
+++ knightsTravails.js
@@ -10,11 +10,39 @@ function knightMoves(start, end) {
     [2, -1],
   ];
 
-  function isInsideBorad([x, y]) {
+  function isInsideBoard([x, y]) {
     return x >= 0 && x < 8 && y >= 0 && y < 8;
   }
 
   // [direction, path]
   const queue = [[start, [start]]];
   const visited = new Set([start.toString()]);
+
+  while (queue.length) {
+    const [current, path] = queue.shift();
+    const [x, y] = current;
+
+    if (x == end[0] && y == end[1]) {
+      console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+      path.forEach((element) => {
+        console.log(element);
+      });
+      return path;
+    }
+
+    for (let [dx, dy] of moves) {
+      const next = [x + dx, y + dy];
+      const key = next.toString();
+
+      if (isInsideBoard(next) && !visited.has(key)) {
+        visited.add(key);
+        queue.push([next, [...path, next]]);
+      }
+    }
+  }
+
+  return [];
 }
+
+// Example
+knightMoves([0, 0], [3, 3]);

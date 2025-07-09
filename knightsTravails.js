ad964d966437215e556b986bfa710a0d03f6f869
arashad202 <ahm.rashad.202@gmail.com>
2025-07-09 05:34:04 +0300

    KnightMoves function with available moves and isInside board check

---
+++ knightsTravails.js
@@ -0,0 +1,16 @@
+function knightMoves(start, end) {
+  const moves = [
+    [2, 1],
+    [1, 2],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [2, -1],
+  ];
+
+  function isInsideBorad([x, y]) {
+    return x >= 0 && x < 8 && y >= 0 && y < 8;
+  }
+}
